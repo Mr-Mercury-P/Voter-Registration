6d4b1c3cfbe8e3f3435a10b7e9e62000524ea394
Puli Prabhas Reddy <141412556+Mr-Mercury-P@users.noreply.github.com>
2024-01-08 22:02:36 +0530

    Create Voter Class.java
    It only contains Voter class and details to be filled

---
+++ Voter Class.java
@@ -0,0 +1,27 @@
+class Voter {
+    private String user;
+    private String password;
+    private String name;
+    private boolean isvoted;
+    Voter(String user, String password, String name) {
+        this.user = user;
+        this.password = password;
+        this.name = name;
+        this.isvoted = false;
+    }
+    String getUser() {
+        return user;
+    }
+    String getPassword() {
+        return password;
+    }
+    String getName() {
+        return name;
+    }
+    boolean voted() {
+        return isvoted;
+    }
+    void setIsvoted(boolean isvoted) {
+        this.isvoted = isvoted;
+    }
+}
